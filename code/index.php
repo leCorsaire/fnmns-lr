a330d113faeb32d9d426c804fbf7af9a1e0128dd
Sam <Sam@MacBook-Pro-de-Samuel.local>
2015-10-25 19:20:07 +0100

    màj

---
+++ code/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE>
+<html lang="fr-fr">
+	<head>
+		<title>Accueil - FNMNS LR</title>
+		<meta charset="utf-8">
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+		
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="css/s_index.css">
+		<link rel="stylesheet" href="css/s_fonts.css">
+	
+		<!-- Latest compiled version of jQuery -->
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
+		
+	</head>
+	<body>
+		<section class="container-fluid">
+			<header class="row">
+				<section id="banner" class="col-xs-12">
+				</section>
+			</header>
+	</section>
+
+		</section>
+	</body>
+</html>
